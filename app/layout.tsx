c8a801a82512023afd4b5bb4ee1e4530c412afc6
nvgrowthhub-a11y <nvgrowthhub@gmail.com>
2026-06-30 15:44:08 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import { Analytics } from '@vercel/analytics/next'
 import type { Metadata, Viewport } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
-import FacebookPixel from '@/components/FacebookPixel';
+import FacebookPixel from '@/components/FacebookPixel'
 import './globals.css'
 
 const geistSans = Geist({ variable: '--font-geist-sans', subsets: ['latin'] })
@@ -38,12 +38,10 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} bg-pink-50`}
     >
       <body className="font-sans antialiased">
-        <FacebookPixel />
-
-        {children}
-
-        {process.env.NODE_ENV === 'production' && <Analytics />}
-      </body>
+  <FacebookPixel />
+  {children}
+  {process.env.NODE_ENV === 'production' && <Analytics />}
+</body>
     </html>
   )
 }
